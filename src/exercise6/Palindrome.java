937c53b9c414619cc751d360fa9844555d5cf191
martonikon <>
2021-12-02 22:01:00 +0000

    Some message

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,22 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+        String temp = Integer.toString(number);
+        int[] newGuess = new int[temp.length()];
+        for (int i = 0; i < temp.length(); i++)
+        {
+          newGuess[i] = temp.charAt(i) - '0';
+        }
+
+        boolean flag = true;
+        for(int i = 0; i < newGuess.length; i++) {
+          if(newGuess[i] != newGuess[newGuess.length - i - 1]) {
+            flag = false;
+          }
+        }
+
+        System.out.println("Palindrome: " + String.valueOf(flag));
     }
 }
--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,22 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int passLength;
+        int digitCount = 0;
+        boolean passValid = true;
+        do {
+            passValid = true;
+            String password = input.next();
+            passLength = password.length();
+            for (int i = 0; i < passLength; i++) {
+                if (!Character.isLetterOrDigit(password.charAt(i))) {
+                    passValid = false;
+                    break;
+                } else if (Character.isDigit(password.charAt(i))) {
+                    digitCount++;
+                }
+            }
+        } while (!passValid || digitCount < 2 || passLength < 8);
+        System.out.println("Password valid!");
     }
 }
